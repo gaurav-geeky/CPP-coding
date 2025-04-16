cd8f3f072b3455d7631834d70bc33becc3467400
gaurav-geeky <gauravmpreat@gmail.com>
2025-04-16 17:32:10 +0530

    updat

---
+++ com.cpp/16april.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+    
+    int a=3; 
+    int b=8; 
+
+    cout<<"multiply of a & b : "<<a*b<<"\t"; 
+    cout<<"sum of a & b : "<<a+b<<endl; 
+    cout<<"differene of a & b : "<<a-b<<endl; 
+
+}
